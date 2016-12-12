fb2c2fefaec1ca0eb925e79c5a82ab57944b9b5c
erismetis <git@erismetis.name>
2016-12-11 21:37:34 -0500

    fix compiler warnings + makefiles

--- Makefile
+++ Makefile
@@ -1,25 +1,36 @@
+PREFIX = /usr/local
 SOURCES = z80.c
-FLAGS = -fPIC -Wall -ansi -g
+OBJS = z80.o
+FLAGS = -fPIC -Wall
 
 force: clean all
 
+.PHONY: all
 all: libz80.so
 
-libz80.so: z80.h $(OBJS)
-	cd codegen && make opcodes
-	gcc $(FLAGS) -shared -o libz80.so $(SOURCES)
+.PHONY: codegen
+codegen:
+	make -C codegen
 
+libz80.so: $(OBJS)
+	gcc $(FLAGS) -shared -o libz80.so $(OBJS)
+
+z80.o: z80.c z80.h codegen
+
+.PHONY: install
 install:
-	install -m 666 libz80.so /usr/lib
-	install -m 666 z80.h /usr/include
+	install -m 666 libz80.so $(PREFIX)/lib
+	install -m 666 z80.h $(PREFIX)/include
 
+.PHONY: clean
 clean:
 	rm -f *.o *.so core
 	cd codegen && make clean
 
+.PHONY: realclean
 realclean: clean
 	rm -rf doc
 
+.PHONY: doc
 doc:	*.h *.c
 	doxygen
-
--- codegen/mktables.c
+++ codegen/mktables.c
@@ -11,6 +11,7 @@
  * This utility generates most of the emulation code from the opcode specification
  *
  */
+#include "ctype.h"
 #include "stdio.h"
 #include "regex.h"
 #include "stdlib.h"
@@ -42,7 +43,7 @@ typedef unsigned char byte;
 void fatal(char* s)
 {
 	printf("%s\n", s);
-	exit(1);	
+	exit(1);
 }
 
 
@@ -50,7 +51,7 @@ void fatal(char* s)
 void fatal2(char* s, char* s2)
 {
 	printf("%s%s\n", s, s2);
-	exit(1);	
+	exit(1);
 }
 
 
@@ -60,10 +61,10 @@ void substAny (char* str, char* set, char r)
 	while (*str)
 	{
 		if (strchr(set, *str))
-			*str = r;	
-	
-		str++;	
-	}	
+			*str = r;
+
+		str++;
+	}
 }
 
 
@@ -74,7 +75,7 @@ void substStr(char* str, char* src, char* rep)
 	char tmp1[MAX_LINE];
 	char* p;
 	int l = strlen(src);
-	
+
 	strcpy(tmp, str);
 	while ( (p = strstr(tmp, src)) != NULL)
 	{
@@ -84,7 +85,7 @@ void substStr(char* str, char* src, char* rep)
 		strcat(tmp1, p + l);
 		strcpy(tmp, tmp1);
 	}
-	
+
 	strcpy(str, tmp);
 }
 
@@ -94,7 +95,7 @@ void fixName (char* opc, char* name)
 {
 	char* p, *q, *r;
 	int l;
-	
+
 	strcpy(name, opc);
 	substAny(name, " +,'", '_');
 	substStr(name, "(", "off_");
@@ -107,7 +108,7 @@ void trim (char* s)
 {
 	while (*s)
 	{
-		if ((*s == 0x0D) || (*s == 0x0A))	
+		if ((*s == 0x0D) || (*s == 0x0A))
 			*s = 0;
 		s++;
 	}
@@ -119,8 +120,8 @@ FILE* openOrDie(char* name, char* mode)
 {
 	FILE* file = fopen(name, mode);
 	if (!file)
-		fatal2("Can't open ", name);	
-	
+		fatal2("Can't open ", name);
+
 	return file;
 }
 
@@ -133,7 +134,7 @@ typedef struct
 {
 	regex_t	re;
 	char*	pat;
-	char*	line[MAX_LINES];	
+	char*	line[MAX_LINES];
 } Item;
 
 
@@ -146,25 +147,25 @@ int readSpec (FILE* in)
 {
 	char line[MAX_LINE];
 	int nLine;
-	
+
 	items = calloc(MAX_REGEX, sizeof(Item));
 	if (!items)
 		fatal("Not enough memory");
-		
+
 	nItems = -1;
 	do
 	{
 		if (feof(in))
 			break;
-			
+
 		fgets(line, MAX_LINE, in);
 		trim(line);
 		if (line[0] == '#')	/* Comment */
 		{
-			
+
 		}
 		else if (line[0] != '\t')	/* New pattern definition */
-		{		
+		{
 			strcat(line, "$");
 			nItems++;
 			if (regcomp(&items[nItems].re, line, REG_EXTENDED))
@@ -178,9 +179,9 @@ int readSpec (FILE* in)
 				items[nItems].line[nLine++] = strdup(line);
 		}
 	} while(1);
-	
+
 	nItems++;
-	
+
 	return nItems+1;
 }
 
@@ -190,7 +191,7 @@ int readSpec (FILE* in)
 void printItem (Item* item)
 {
 	char** s = item->line;
-	
+
 	printf("%s\n", item->pat);
 	while (*s)
 	{
@@ -207,21 +208,21 @@ void printItem (Item* item)
 /** Handles opcode-in-code */
 int printCall (char* s, FILE* code)
 {
-	char tmp[MAX_LINE];	
+	char tmp[MAX_LINE];
 	char* p = tmp;
-	
+
 	strcpy(tmp, s);
-	
+
 	while (isspace(*p) && (*p))
 		p++;
-		
+
 	if (*p != '%')
 		return 0;
-	
+
 	*p = 0;
 	fprintf(code, "%s", tmp);
 	p++;
-	
+
 	fixName(&s[p-tmp], tmp);
 	fprintf(code, "%s(ctx);\n", tmp);
 	return 1;
@@ -245,26 +246,26 @@ void generateCodeTable (FILE* opcodes, FILE* code)
 	printf("Generating opcode implementations...");
 	last[0] = 0;
 	do
-	{			
+	{
 		fgets(line, MAX_LINE, opcodes);
 		trim(line);
-		
+
 		if (feof(opcodes))
 			break;
-				
+
 		for (q = line, p = q+OPCODE_OFFSET; *p; *q++ = *p++);	/* Skip the hex part */
 		*q = 0;
-		
+
 		/* Avoid duplicate opcodes */
 		if (strcmp(last, line) == 0)
-			continue;		
+			continue;
 		strcpy(last, line);
-			
+
 		/* Find the appropriate pattern */
 		for (i = 0; i < nItems; i++)
 		{
 			if (regexec(&items[i].re, line, MAX_MATCH, matches, REG_EXTENDED) == 0)
-			{	
+			{
 				if (matches[0].rm_so == 0)	/* Match only at beginning of line */
 				{
 					/*printf("%s : match %s\n", &line, items[i].pat);*/
@@ -272,16 +273,16 @@ void generateCodeTable (FILE* opcodes, FILE* code)
 				}
 			}
 		}
-		
+
 		if (i >= nItems)
 			fatal2(line, " didn't match anything");
-			
+
 		item = &items[i];
-		
+
 		/* Print function stub */
 		fixName(line, name);
 		fprintf(code, "static void %s (Z80Context* ctx)\n{\n", name);
-				
+
 		/* Substitute submatches in each output line and print the code */
 		cmds = item->line;
 		strcpy(parm, "%0");
@@ -291,21 +292,21 @@ void generateCodeTable (FILE* opcodes, FILE* code)
 			{
 				strncpy(tmp, *cmds, MAX_LINE);
 				q = tmp;
-		
+
 				for (i = 1; i < MAX_MATCH; i++)
 				{
 					parm[1] = i + '0';
 					strncpy(subst, &line[matches[i].rm_so], matches[i].rm_eo - matches[i].rm_so);
 					subst[matches[i].rm_eo - matches[i].rm_so] = 0;
-					
+
 					substStr(tmp, parm, subst);
 				}
 				fprintf(code, "%s\n", tmp);
 			}
-			
-			cmds++;	
+
+			cmds++;
 		}
-		
+
 		fprintf(code, "}\n\n\n");
 	} while(1);
 	printf("done\n");
@@ -315,21 +316,21 @@ void generateCodeTable (FILE* opcodes, FILE* code)
 void generateCode (void)
 {
 	FILE* spec, *opcodes, *code, *table;
-	
+
 	/* ----- Read the specification ----- */
 	printf("Reading specification...");
 
 	spec = openOrDie(OPCODES_SPEC, "rb");
 	if (!readSpec(spec))
-		fatal("Error reading the specification");	
+		fatal("Error reading the specification");
 	fclose(spec);
 	printf("done\n");
 
 	opcodes = openOrDie(OPCODES_LIST, "rb");
 	code = openOrDie(OPCODES_IMPL, "wb");
-		
+
 	generateCodeTable(opcodes, code);
-	
+
 	fclose(opcodes);
 	fclose(code);
 }
@@ -345,13 +346,13 @@ void mkFormat(char* src, char* dst)
 	substStr(dst, "d", "0%02Xh");
 	substStr(dst, "e", "%d");
 	substStr(dst, "nn", "0%04Xh");
-	substStr(dst, "n", "0%02Xh");	
+	substStr(dst, "n", "0%02Xh");
 }
 
 
 int isHex(char c)
 {
-	return (isxdigit(c) && isupper(c)) || isdigit(c);	
+	return (isxdigit(c) && isupper(c)) || isdigit(c);
 }
 
 
@@ -359,7 +360,7 @@ int hexVal(char c)
 {
 	if (isdigit(c))
 		return c - '0';
-	return c - 'A' + 10;	
+	return c - 'A' + 10;
 }
 
 
@@ -371,7 +372,7 @@ typedef enum
 	OP_NONE,
 	OP_BYTE,
 	OP_OFFSET,
-	OP_WORD	
+	OP_WORD
 } Z80OperandType;
 
 char* OpTypeName[] = { "OP_NONE", "OP_BYTE", "OP_OFFSET", "OP_WORD" };
@@ -383,10 +384,10 @@ struct Z80OpcodeEntry
 	int pre_skip;
 	int post_skip;
 	char* func;		/*	Z80OpcodeFunc* func;*/
-	
+
 	int operand_type;
-	char* format;	
-	
+	char* format;
+
 	struct Z80OpcodeTable* table;
 };
 
@@ -429,7 +430,7 @@ char* nextToken(char* cur, byte* val, TokenType* ttype)
 		}
 		else if (*cur == 'd')
 		{
-			*ttype = TT_D;	
+			*ttype = TT_D;
 			cur++;
 			break;
 		}
@@ -456,7 +457,7 @@ char* nextToken(char* cur, byte* val, TokenType* ttype)
 			fatal2("Unknown char	at ", cur);
 		}
 	};
-	
+
 	return cur;
 }
 
@@ -470,17 +471,17 @@ struct Z80OpcodeTable* createTableTree(FILE* opcodes, FILE* table)
 	char tmp[MAX_LINE];
 	char* cur;
 	byte code;
-	TokenType tt;	
+	TokenType tt;
 	byte prefix[10];
 	int prefixLen, i;
 	int wasOperand;
 	int opcodeOffset;
-	
+
 	mainTable = calloc(1, sizeof(struct Z80OpcodeTable));
 	mainTable->name = strdup("");
-	
+
 	fprintf(table, "static struct Z80OpcodeTable opcodes_main;\n");
-	
+
 	rewind(opcodes);
 
 	printf("Determining opcode prefixes...");
@@ -488,14 +489,14 @@ struct Z80OpcodeTable* createTableTree(FILE* opcodes, FILE* table)
 	{
 		fgets(line, MAX_LINE, opcodes);
 		trim(line);
-		
+
 		if (feof(opcodes))
 			break;
-			
+
 		prefixLen = 0;
 		line[OPCODE_OFFSET - 1] = 0;
-	
-		/* Get the codes */	
+
+		/* Get the codes */
 		wasOperand = 0;
 		opcodeOffset = 0;
 		cur = line;
@@ -514,10 +515,10 @@ struct Z80OpcodeTable* createTableTree(FILE* opcodes, FILE* table)
 			}
 			else
 			{
-				wasOperand = 1;	
+				wasOperand = 1;
 			}
 		} while(1);
-	
+
 		/* All hex codes except the last one form the prefix of the instruction */
 		current = mainTable;
 		for (i = 0; i < prefixLen - 1; i++)
@@ -529,9 +530,9 @@ struct Z80OpcodeTable* createTableTree(FILE* opcodes, FILE* table)
 				sprintf(tmp, "%s%02X", current->name, code);
 				current->entries[code].table = calloc(1, sizeof(struct Z80OpcodeTable));
 				current = current->entries[code].table;
-				current->name = strdup(tmp);				
+				current->name = strdup(tmp);
 				fprintf(table, "static struct Z80OpcodeTable opcodes_%s;\n", current->name);
-				
+
 				printf("%s ", tmp);
 			}
 			else
@@ -539,12 +540,12 @@ struct Z80OpcodeTable* createTableTree(FILE* opcodes, FILE* table)
 				current = current->entries[code].table;
 			}
 		}
-		
+
 		current->opcode_offset = opcodeOffset;
 	} while(1);
-	
+
 	printf("\n");
-	
+
 	mainTable->name = strdup("main");
 	return mainTable;
 }
@@ -561,21 +562,21 @@ void scanOpcodes(FILE* opcodes, struct Z80OpcodeTable* mainTable)
 	struct Z80OpcodeTable* current;
 	struct Z80OpcodeEntry* ent;
 	int opType;
-	
-	rewind(opcodes);	
+
+	rewind(opcodes);
 	do
 	{
 		fgets(line, MAX_LINE, opcodes);
 		trim(line);
-		
+
 		if (feof(opcodes))
 			break;
-			
+
 		mkFormat(&line[OPCODE_OFFSET], fmt);
 		fixName(&line[OPCODE_OFFSET], name);
 		line[OPCODE_OFFSET] = 0;
-	
-		current = mainTable;		
+
+		current = mainTable;
 		cur = line;
 		opType = OP_NONE;
 		do
@@ -592,10 +593,10 @@ void scanOpcodes(FILE* opcodes, struct Z80OpcodeTable* mainTable)
 					current = current->entries[code].table;
 					continue;
 				}
-				
+
 				ent = &current->entries[code];
 				ent->func = strdup(name);
-				ent->format = strdup(fmt);				
+				ent->format = strdup(fmt);
 			}
 			else if (tt == TT_NN)
 			{
@@ -610,9 +611,9 @@ void scanOpcodes(FILE* opcodes, struct Z80OpcodeTable* mainTable)
 				opType = OP_OFFSET;
 			}
 		} while(1);
-		
+
 		ent->operand_type = opType;
-			
+
 	} while (1);
 }
 
@@ -623,11 +624,11 @@ void outputTable(struct Z80OpcodeTable* table, FILE* file)
 	struct Z80OpcodeEntry* opc;
 	struct Z80OpcodeTable* tbl;
 	char fmt[MAX_LINE];
-	
+
 	printf("Outputting table %s...", table->name);
-	
+
 	fprintf(file, "static struct Z80OpcodeTable opcodes_%s = { %d, {\n", table->name, table->opcode_offset);
-	
+
 	for (i = 0, opc = table->entries; i < 256; i++, opc++)
 	{
 		tbl = opc->table;
@@ -646,19 +647,19 @@ void outputTable(struct Z80OpcodeTable* table, FILE* file)
 												);
 
 	}
-	fprintf(file, "} };\n");	
+	fprintf(file, "} };\n");
 	fprintf(file, "\n\n");
-		
+
 	printf("done\n");
-	
+
 	for (i = 0, opc = table->entries; i < 256; i++, opc++)
 	{
-		tbl = opc->table;			
+		tbl = opc->table;
 		if (tbl)
 			outputTable(tbl, file);
 	}
 }
-	
+
 
 void generateParserTables(FILE* opcodes, FILE* table)
 {
@@ -672,12 +673,12 @@ void generateParserTables(FILE* opcodes, FILE* table)
 void generateParser(void)
 {
 	FILE* table, *opcodes;
-	
+
 	opcodes = openOrDie(OPCODES_LIST, "rb");
 	table = openOrDie(OPCODES_TABLE, "wb");
-	
+
 	generateParserTables(opcodes, table);
-	
+
 	fclose(table);
 	fclose(opcodes);
 }
@@ -689,4 +690,3 @@ int main (void)
 	generateParser();
 	return 0;
 }
-
